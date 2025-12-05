52cafa277cf0d1c5f7e21fbb43cc1d216f363f90
Harry_Winchester <winchesterhj@gmail.com>
2025-12-05 00:11:15 +0000

    crossword?

---
+++ Crossword/src/Crossword.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class Crossword {
+    public int height;
+    public int width;
+    public int[][] structure;
+    public String[] words;
+    public int[] variables;
+    public Map<String, String> overlaps;
+
+    public Crossword(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+    public void display() {
+
+    }
+
+    public String[] 
+}
